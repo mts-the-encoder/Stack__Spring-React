3df44ede4c396c7f87bd0b230753a7621d5771fd
zmtheus <matheus.nsantos@bandtec.com.br>
2022-08-28 14:57:54 -0300

    add lombok ao projeto

--- my-finances/finances/src/main/java/com/mts/finances/models/LaunchModel.java
+++ my-finances/finances/src/main/java/com/mts/finances/models/LaunchModel.java
@@ -1,5 +1,7 @@
 package com.mts.finances.models;
 
+import lombok.Builder;
+import lombok.Data;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import javax.persistence.*;
@@ -9,6 +11,8 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "launch", schema = "finances")
+@Builder
+@Data
 public class LaunchModel {
 
     @Id
@@ -43,111 +47,7 @@ public class LaunchModel {
     @Enumerated(value = EnumType.STRING)
     private LaunchStatus status;
 
-    @Override
-    public String toString() {
-        return "LaunchModel{" +
-                "id=" + id +
-                ", description='" + description + '\'' +
-                ", month='" + month + '\'' +
-                ", year=" + year +
-                ", user=" + user +
-                ", value=" + value +
-                ", registrationDate=" + registrationDate +
-                ", type=" + type +
-                ", status=" + status +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        LaunchModel that = (LaunchModel) o;
-        return Objects
-                .equals(id, that.id) && Objects
-                .equals(description, that.description) && Objects
-                .equals(month, that.month) && Objects
-                .equals(year, that.year) && Objects
-                .equals(user, that.user) && Objects
-                .equals(value, that.value) && Objects
-                .equals(registrationDate, that.registrationDate) &&
-                    type == that.type && status == that.status;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, description, month, year, user, value, registrationDate, type, status);
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getMonth() {
-        return month;
-    }
-
-    public void setMonth(String month) {
-        this.month = month;
-    }
-
-    public Integer getYear() {
-        return year;
-    }
-
-    public void setYear(Integer year) {
-        this.year = year;
-    }
-
-    public UserModel getUser() {
-        return user;
-    }
-
-    public void setUser(UserModel user) {
-        this.user = user;
-    }
-
-    public BigDecimal getValue() {
-        return value;
-    }
-
-    public void setValue(BigDecimal value) {
-        this.value = value;
-    }
-
-    public LocalDate getRegistrationDate() {
-        return registrationDate;
-    }
-
-    public void setRegistrationDate(LocalDate registrationDate) {
-        this.registrationDate = registrationDate;
-    }
-
-    public LaunchType getType() {
-        return type;
-    }
-
-    public void setType(LaunchType type) {
-        this.type = type;
-    }
-
-    public LaunchStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(LaunchStatus status) {
-        this.status = status;
+    public LaunchModel() {
+        
     }
 }
