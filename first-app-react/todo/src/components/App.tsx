b057d6f70bec93d059eb6bead8f9dd5f27317e7e
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-09-17 02:03:35 -0300

    navbar

---
+++ first-app-react/todo/src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const App = () => {
+    return (
+    <div>
+        <Navbar/>
+        <h2>1st App</h2>
+    </div>
+    ); 
+}
+
+export default App;
